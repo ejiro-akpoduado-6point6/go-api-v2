b937dad130ba753685853071f5fb40193487bf17
Ejiro Akpoduado <ejiro.akpoduado@6point6.co.uk>
2022-05-23 09:03:33 +0100

    added postgres

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,11 +1,33 @@
 package main
 
-import "fmt"
+import (
+	// "context"
+	"fmt"
+
+	"github.com/TutorialEdge/go-rest-api-course/internal/db"
+)
 
 //Run - si responsible for the instantiation and
 //startup for our go application
 func Run() error {
 	fmt.Println("starting up our application")
+
+	db, err := db.NewDatabase()
+	if err != nil {
+		fmt.Println("Failed to connect to the database")
+		return err
+	}
+	// if err := db.Ping(context.Background()); err != nil {
+	// 	return err
+	// }
+
+	if err := db.MigrateDB(); err != nil {
+		fmt.Println("failed to migrate database")
+		return err
+	}
+
+	fmt.Println("successfully connected and pinged database")
+
 	return nil
 }
 
--- go.mod
+++ go.mod
@@ -1,3 +1,14 @@
 module github.com/TutorialEdge/go-rest-api-course
 
 go 1.18
+
+require (
+	github.com/golang-migrate/migrate/v4 v4.15.2 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/jmoiron/sqlx v1.3.5 // indirect
+	github.com/lib/pq v1.10.6 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	golang.org/x/sys v0.0.0-20220317061510-51cd9980dadf // indirect
+)
